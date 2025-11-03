a465696e122af7b9c8bcff66189d586e6fc2d9f8
luisjuarezt16 <luisjuarezt16@gmail.com>
2025-11-03 18:15:56 -0300

    arreglos

--- js/edit.js
+++ js/edit.js
@@ -1,59 +1,48 @@
-let reservas = JSON.parse(localStorage.getItem("reservas")) || []
+let reservas = JSON.parse(localStorage.getItem('reservas')) || []
 
-const nombre = document.getElementById ("nombreempresa")
-const visita = document.getElementById ("fechavisita")
-const hora = document.getElementById ("hora")
-const problema = document.getElementById ("descripcionproblema")
-const btnreserva = document.getElementById ("btnreservar")
-const btnlog = document.getElementById ("btnlog")
+const nombre = document.getElementById('nombreempresa')
+const fecha = document.getElementById('fechavisita')
+const hora = document.getElementById('hora')
+const problema = document.getElementById('descripcionproblema')
+const btnreserva = document.getElementById('btnreservar')
+const btnlog = document.getElementById('btnlog')
 
+// Pillamos la ID del localStorage
+const IDReserva = localStorage.getItem('reservaElegida')
+// Pillamos la lista de reservas
+const listaDeReservas = JSON.parse(localStorage.getItem('reservas'))
+// Pillamos la reserva que vamos a editar
+const reserva = listaDeReservas[IDReserva]
 
-const IDReserva = localStorage.getItem ("reservaElegida")
-
-const listaDeReservas = JSON.parse(localStorage.getItem("reservas"))
-
-const reserva = listaDeReservas [IDReserva]
-
-
+// Asignamos los valores de los inputs con los valores del objecto
 nombre.value = reserva.empresa
-hora.value = reserva.empresa
-visita.value = reserva.visita
+hora.value = reserva.hora
+fecha.value = reserva.fecha
 problema.value = reserva.problema
 
-
-btnlog.addEventListener ("click", ()=> {
-   
-    location.href = "../login.html"
-
+btnreserva.addEventListener('click', () => {
+  location.href = './reservas.html'
 })
 
+btnreserva.addEventListener('click', () => {
+  const reserva = {
+    empresa: nombre.value,
+    fecha: fecha.value,
+    hora: hora.value,
+    problema: problema.value,
+  }
 
-btnreserva.addEventListener ("click", ()=> {
+  // Editamos el item a editar en la lista de reservas
+  reservas[IDReserva] = reserva
+  // Reemplazamos la lista entera en el local Storage
+  localStorage.setItem('reservas', JSON.stringify(reservas))
+  // Borramos la reserva elegida del local Storage
+  localStorage.setItem('reservaElegida', '')
 
-    const reserva = {
-        nombre: nombre.value,
-        fecha: visita.value,
-        hora: hora.value,
-        problema: problema.value,
+  nombre.value = ''
+  fecha.value = ''
+  hora.value = ''
+  problema.value = ''
 
-    }
-    
-
-    reservas [IDReserva] = reserva 
-
-    localStorage.setItem("reservas", JSON.stringify(reservas))
-
-    localStorage.setItem("reservaElegida", "")
-
-
-    nombre.value = ""
-    visita.value = ""
-    hora.value = ""
-    problema.value = ""
-
-
-    location.href = "./reservas.html"
-
+  location.href = './reservas.html'
 })
-
-
--- js/reservas.js
+++ js/reservas.js
@@ -1,28 +1,20 @@
-let reservas = JSON.parse(localStorage.getItem("reservas")) || []
+let reservas = JSON.parse(localStorage.getItem('reservas')) || [];
 
-const contenedor = document.getElementById("contenedor-reservas")
+const contenedor = document.getElementById('contenedor-reservas');
 
-const crearnuevo = document.getElementById ("crearreserva")
+const crearnuevo = document.getElementById('crearreserva');
 
-const borrartodo = document.getElementById ("borrartodo")
+const borrartodo = document.getElementById('borrartodo');
 
-
-
-
-crearnuevo.addEventListener ("click", () => {
-
-    location.href = "../index.html"
-
-})
-
-
-
+crearnuevo.addEventListener('click', () => {
+  location.href = '../index.html';
+});
 
 function mostrarReservas() {
-    contenedor.innerHTML = ""
+  contenedor.innerHTML = '';
 
-    reservas.forEach((r, i) => {
-        contenedor.innerHTML += `
+  reservas.forEach((r, i) => {
+    contenedor.innerHTML += `
             <div class="col-lg-4 justify-content-center g-3">
                 <div class="tarjeta-reserva">
                     <h3>Reserva #${i + 1}</h3>
@@ -30,49 +22,38 @@ function mostrarReservas() {
                     <p>Fecha: ${r.fecha}</p>
                     <p>Hora: ${r.hora}</p>
                     <p>Problema: ${r.problema}</p>
-                    <button type="button" id="btnborrar-${i}" class="btnlog col-lg-4 mb-5">BORRAR</button>
-                    <button type="button" id="botonEditar-${i}" class="btnlog col-lg-4 mb-5">EDITAR</button>
+                    <button type="button" id="btnborrar-${i}" class="btnlog col-lg-4 mb-5">BORRAR RESERVA</button>
+                    <button type="button" id="btneditar-${i}" class="btnlog col-lg-4 mb-5">EDITAR RESERVA</button>
                 </div>
-            </div>
-        `
-    })
 
+        `;
+  });
 
-    for (let i = 0; i < reservas.length; i++) {
-        const botonBorrar = document.getElementById(`btnborrar-${i}`)
+  for (let i = 0; i < reservas.length; i++) {
+    const botonBorrar = document.getElementById(`btnborrar-${i}`);
+    const botonEditar = document.getElementById(`btneditar-${i}`);
 
-        botonBorrar.addEventListener("click", () => {
-        let nuevasReservas = []
-        for (let j = 0; j < reservas.length; j++) {
-            if (j !== i) {
-            nuevasReservas.push(reservas[j])
-            }
+    botonBorrar.addEventListener('click', () => {
+      let nuevasReservas = [];
+      for (let j = 0; j < reservas.length; j++) {
+        if (j !== i) {
+          nuevasReservas.push(reservas[j]);
         }
+      }
 
+      reservas = nuevasReservas;
+      localStorage.setItem('reservas', JSON.stringify(reservas));
+      mostrarReservas();
+    });
 
-      
-
-        reservas = nuevasReservas
-        localStorage.setItem("reservas", JSON.stringify(reservas))
-        mostrarReservas()
-        })
-
-        
-
-        botonEditar.addEventListener ("click", () => {
-            localStorage.setItem("reservaElegida", i)
-            location.href = "../pages/editar.html"
-
-        })
-
-
-    }
-
-    
+    botonEditar.addEventListener('click', () => {
+      localStorage.setItem('reservaElegida', i);
+      location.href = '../pages/editar.html';
+    });
+  }
 }
 
-mostrarReservas()
-
+mostrarReservas();
 
 
 
