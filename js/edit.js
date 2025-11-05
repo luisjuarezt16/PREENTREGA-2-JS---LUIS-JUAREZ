ed84b85fdcc18c879a24b1792f24dfa86c476a18
luisjuarezt16 <luisjuarezt16@gmail.com>
2025-11-05 12:38:50 -0300

    detalles

--- js/edit.js
+++ js/edit.js
@@ -7,14 +7,13 @@ const problema = document.getElementById('descripcionproblema')
 const btnreserva = document.getElementById('btnreservar')
 const btnlog = document.getElementById('btnlog')
 
-// Pillamos la ID del localStorage
 const IDReserva = localStorage.getItem('reservaElegida')
-// Pillamos la lista de reservas
+
 const listaDeReservas = JSON.parse(localStorage.getItem('reservas'))
-// Pillamos la reserva que vamos a editar
+
 const reserva = listaDeReservas[IDReserva]
 
-// Asignamos los valores de los inputs con los valores del objecto
+
 nombre.value = reserva.empresa
 hora.value = reserva.hora
 fecha.value = reserva.fecha
@@ -28,11 +27,11 @@ btnreserva.addEventListener('click', () => {
     problema: problema.value,
   }
 
-  // Editamos el item a editar en la lista de reservas
+
   reservas[IDReserva] = reserva
-  // Reemplazamos la lista entera en el local Storage
+
   localStorage.setItem('reservas', JSON.stringify(reservas))
-  // Borramos la reserva elegida del local Storage
+
   localStorage.setItem('reservaElegida', '')
 
   nombre.value = ''
